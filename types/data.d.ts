ed512ec846d9fd00f311bcec1e1da9a6a0443420
Arafat <arafat.ahmed@yaana.com>
2025-04-02 22:16:59 +0600

    update

--- types/data.d.ts
+++ types/data.d.ts
@@ -1,28 +1,29 @@
 export type CasualtyType =
-	| 'Injury'
-	| 'No Casualties'
-	| 'Multiple Casualties'
-	| 'Death';
+  | "Injury"
+  | "No Casualties"
+  | "Multiple Casualties"
+  | "Death";
 
 export interface NewsLink {
-	title: string;
-	url: string;
+  title: string;
+  url: string;
 }
 
 export interface CasualtyPerson {
-	id: number;
-	date: number | null; // Epoch timestamp can be null
-	name: string | null;
-	age: number | null;
-	occupation: string | null;
-	type: CasualtyType | null;
-	location: string | null;
-	'Location Coordinates'?: string | null;
-	lat: number | null;
-	lng: number | null;
-	description?: string | null;
-	incidentDetails?: string | null;
-	extendedDetails?: string | null;
-	newsLinks?: NewsLink[] | null;
-	image?: string | null;
+  id: number;
+  date: number | null; // Epoch timestamp can be null
+  name: string | null;
+  age: number | null;
+  occupation: string | null;
+  type: CasualtyType | null;
+  location: string | null;
+  "Location Coordinates"?: string | null;
+  lat: number | null;
+  lng: number | null;
+  description?: string | null;
+  incidentDetails?: string | null;
+  extendedDetails?: string | null;
+  newsLinks?: NewsLink[] | null;
+  image?: string | null;
+  gender?: string | null;
 }
