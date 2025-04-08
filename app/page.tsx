41a8c3c978c5422829d838b2976e95dd02eeeb13
Arafat <arafat.ahmed@yaana.com>
2025-04-08 19:38:13 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,12 @@ import {
 import { useFilterStore } from "@/lib/filter-store";
 import { useIncidentStore } from "@/lib/incident-store";
 import { useSidebarStore } from "@/lib/sidebar-store";
+import {
+  getTotalCases,
+  getTotalDeaths,
+  getTotalInjuries,
+} from "@/lib/data_district_wise_injury_death";
+import DonutChart from "@/components/ui/donut-chart";
 
 export default function Home() {
   const { casualtyTypeFilter, setCasualtyTypeFilter, resetFilters } =
@@ -31,6 +37,28 @@ export default function Home() {
       setCasualtyTypeFilter(casualtyType);
     }
   };
+
+  const deathsDonutChartConfig = [
+    { label: "Deaths", value: getTotalDeaths(), color: "#bb4110" },
+    { label: "Total Casualties", value: getTotalCases(), color: "#e2e0df" },
+  ];
+
+  const deathPercentage =
+    (
+      (getTotalDeaths() / (getTotalDeaths() + getTotalInjuries())) *
+      100
+    ).toFixed(1) + "%";
+
+  const injuriesDonutChartConfig = [
+    { label: "Injuries", value: getTotalInjuries(), color: "#d8af81" },
+    { label: "Total Casualties", value: getTotalCases(), color: "#e2e0df" },
+  ];
+
+  const injuriesPercentage =
+    (
+      (getTotalInjuries() / (getTotalDeaths() + getTotalInjuries())) *
+      100
+    ).toFixed(1) + "%";
 
   return (
     <main className="flex flex-1 flex-col bg-background h-[100vh]">
@@ -59,15 +87,53 @@ export default function Home() {
           ))}
         </div>
 
-        <div className="text-slate-600 md:w-50 w-full border-r">
-          <div className="flex flex-col  flex-wrap p-10">
-            <h5 className="text-2xl font-semibold">July Memorial</h5>
-            <p className="hidden md:block text-xs font-light">
-              Remembering July 2024
+        <div className="bg-red-200 text-slate-600  absolute z-[1] w-0 ml-[48px]">
+          <div className="flex flex-col p-10">
+            <h5 className="text-xl font-medium w-max">July Memorial</h5>
+            <p className="text-xs font-light w-max">Remembering July 2024</p>
+          </div>
+
+          <div className="flex flex-col p-10">
+            <h5 className="text-xs w-max">Total Casualties</h5>
+            <p className="text-4xl font-semibold w-max">
+              {getTotalCases().toLocaleString()}
             </p>
+
+            <div className="py-10 w-max flex gap-10">
+              <div className="flex gap-2 items-center">
+                <DonutChart
+                  data={deathsDonutChartConfig}
+                  size={56}
+                  thickness={3}
+                  innerText={deathPercentage}
+                />
+
+                <div>
+                  <h1 className="text-xs font-semibold">{getTotalDeaths()}</h1>
+                  <p className="text-xs">Deaths</p>
+                </div>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <DonutChart
+                  data={injuriesDonutChartConfig}
+                  size={56}
+                  thickness={3}
+                  innerText={injuriesPercentage}
+                />
+
+                <div>
+                  <h1 className="text-xs font-semibold">
+                    {getTotalInjuries()}
+                  </h1>
+                  <p className="text-xs">Injuries</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="hidden md:flex md:w-full">
+
+        <div className="hidden md:flex md:w-full border">
           <MapContainer />
         </div>
       </div>
--- constant/map-container-config.ts
+++ constant/map-container-config.ts
@@ -1,19 +1,19 @@
-import type { LatLngExpression } from 'leaflet';
+import type { LatLngExpression } from "leaflet";
 
 export const BANGLADESH_CENTER: LatLngExpression = [23.8103, 90.4125];
 
 export const MAP_ZOOM = {
-	DEFAULT: 7,
-	MAX: 12,
-	MIN: 7,
+  DEFAULT: 7.25,
+  MAX: 12,
+  MIN: 7.25,
 };
 export const MAP_CONTAINER = {
-	center: BANGLADESH_CENTER,
-	zoom: MAP_ZOOM.DEFAULT,
-	zoomControl: false,
-	minZoom: MAP_ZOOM.MIN,
-	dragging: true,
-	doubleClickZoom: true,
-	scrollWheelZoom: true,
-	style: { height: 'calc(100vh)' },
+  center: BANGLADESH_CENTER,
+  zoom: MAP_ZOOM.DEFAULT,
+  zoomControl: false,
+  minZoom: MAP_ZOOM.MIN,
+  dragging: true,
+  doubleClickZoom: true,
+  scrollWheelZoom: true,
+  style: { height: "calc(100vh)" },
 };
