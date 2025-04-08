e0f1fb6a6f08ea147c8a06d1cf51fa0d27751f6b
Arafat <arafat.ahmed@yaana.com>
2025-04-08 11:51:44 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,75 +1,76 @@
-'use client';
+"use client";
 
-import MapContainer from '@/components/map-container';
-import IncidentSidebar from '@/components/incidentSidebar';
-import Sidebar from '@/components/sidebar';
-import Navbar from '@/components/navbar';
-import { Button } from '@/components/ui/button';
-import { CASUALTY } from '@/constant/casualty-types';
+import MapContainer from "@/components/map-container";
+import IncidentSidebar from "@/components/incidentSidebar";
+import Sidebar from "@/components/sidebar";
+import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { CASUALTY } from "@/constant/casualty-types";
 import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from '@/components/ui/tooltip';
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-import { useFilterStore } from '@/lib/filter-store';
-import { useIncidentStore } from '@/lib/incident-store';
-import { useSidebarStore } from '@/lib/sidebar-store';
+import { useFilterStore } from "@/lib/filter-store";
+import { useIncidentStore } from "@/lib/incident-store";
+import { useSidebarStore } from "@/lib/sidebar-store";
 
 export default function Home() {
-	const { casualtyTypeFilter, setCasualtyTypeFilter, resetFilters } =
-		useFilterStore();
-	const { setSelectedIncident } = useIncidentStore();
-	const { close } = useSidebarStore();
+  const { casualtyTypeFilter, setCasualtyTypeFilter, resetFilters } =
+    useFilterStore();
+  const { setSelectedIncident } = useIncidentStore();
+  const { close } = useSidebarStore();
 
-	const handleCasualtyTypeFilter = (casualtyType: string) => {
-		if (casualtyTypeFilter !== casualtyType) {
-			setSelectedIncident(null);
-			resetFilters();
-			close();
-			setCasualtyTypeFilter(casualtyType);
-		}
-	};
+  const handleCasualtyTypeFilter = (casualtyType: string) => {
+    if (casualtyTypeFilter !== casualtyType) {
+      setSelectedIncident(null);
+      resetFilters();
+      close();
+      setCasualtyTypeFilter(casualtyType);
+    }
+  };
 
-	return (
-		<main className="flex flex-1 flex-col bg-background h-[100vh]">
-			<div className="flex h-full">
-				<div className="w-[50px] flex flex-col bg-foreground justify-center items-center shadow-2xl z-[100]">
-					{CASUALTY.map((casualty) => (
-						<TooltipProvider key={casualty.name}>
-							<Tooltip>
-								<TooltipTrigger asChild>
-									<Button
-										variant="ghost"
-										key={casualty.name}
-										onClick={() => handleCasualtyTypeFilter(casualty.value)}
-										className={`text-slate-600 dark:text-white ${
-											casualtyTypeFilter === casualty.value ? 'bg-muted' : ''
-										}`}
-									>
-										<casualty.icon />
-									</Button>
-								</TooltipTrigger>
+  return (
+    <main className="flex flex-1 flex-col bg-background h-[100vh]">
+      {/* <Navbar /> */}
+      <div className="flex h-full">
+        <div className="w-[50px] flex flex-col bg-foreground justify-center items-center shadow-2xl z-[100]">
+          {CASUALTY.map((casualty) => (
+            <TooltipProvider key={casualty.name}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    key={casualty.name}
+                    onClick={() => handleCasualtyTypeFilter(casualty.value)}
+                    className={`text-slate-600 dark:text-white ${
+                      casualtyTypeFilter === casualty.value ? "bg-muted" : ""
+                    }`}
+                  >
+                    <casualty.icon />
+                  </Button>
+                </TooltipTrigger>
 
-								<TooltipContent>{casualty.name}</TooltipContent>
-							</Tooltip>
-						</TooltipProvider>
-					))}
-				</div>
+                <TooltipContent>{casualty.name}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </div>
 
-				<div className="hidden md:flex md:w-full">
-					<div className="text-slate-600 md:w-[50%] w-full">
-						<div className="flex flex-col  flex-wrap p-10">
-							<h5 className="text-2xl font-semibold">July Memorial</h5>
-							<p className="hidden md:block text-xs font-light">
-								Remembering July 2024
-							</p>
-						</div>
-					</div>
-					<MapContainer />
-				</div>
-			</div>
-		</main>
-	);
+        <div className="text-slate-600 md:w-50 w-full border-r">
+          <div className="flex flex-col  flex-wrap p-10">
+            <h5 className="text-2xl font-semibold">July Memorial</h5>
+            <p className="hidden md:block text-xs font-light">
+              Remembering July 2024
+            </p>
+          </div>
+        </div>
+        <div className="hidden md:flex md:w-full">
+          <MapContainer />
+        </div>
+      </div>
+    </main>
+  );
 }
