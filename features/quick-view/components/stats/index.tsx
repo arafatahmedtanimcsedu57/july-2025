d9e1729b76df751e7c399101ddab4bfaded59fc6
Arafat <arafatahmedtanimcsedu57@gmail.com>
2025-04-24 00:53:30 +0600

    update

--- features/quick-view/components/stats/index.tsx
+++ features/quick-view/components/stats/index.tsx
@@ -1,56 +1,37 @@
-import React, { type Dispatch, type SetStateAction } from "react";
-import FilterContainer from "@/app/filter-container";
-import Methodology from "@/app/methodology";
+import React, { type Dispatch, type SetStateAction } from 'react';
+import FilterContainer from '@/app/filter-container';
+import Methodology from '@/app/methodology';
 import {
-  DonutCharts,
-  TabularData,
-  TotalCasualties,
-} from "@/features/quick-view/components/stats/overall";
-import { Button } from "@/shared/ui/button";
-import { ChevronUp } from "lucide-react";
+	DonutCharts,
+	TabularData,
+	TotalCasualties,
+} from '@/features/quick-view/components/stats/overall';
+import { Button } from '@/shared/ui/button';
+import { ChevronUp } from 'lucide-react';
 
 interface StatsProps {
-  show: boolean;
-  setShow: Dispatch<SetStateAction<boolean>>;
+	show: boolean;
+	setShow: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function Stats({ show, setShow }: StatsProps) {
-  return (
-    <div className="h-full flex flex-col min-w-[430px] bg-foreground rounded-3xl border shadow-lg dark:text-white text-slate-700 relative">
-      {/* <Button
-        variant="default"
-        className="hover:bg-primary px-4 !py-0 !rounded-b-none rounded-t-2xl absolute -top-10 left-[50%] -translate-x-[50%]"
-        onClick={() => {
-          setShow((prev) => !prev);
-        }}
-      >
-        <ChevronUp />
-      </Button> */}
+	return (
+		<div className="h-full flex flex-col min-w-[430px] bg-background rounded-3xl border shadow-lg dark:text-white text-slate-700 relative">
+			<TotalCasualties />
 
-      {/* Top section: Total Casualties */}
-      <TotalCasualties />
+			<div className="flex-1 min-h-0 flex flex-col px-10">
+				<div className="flex flex-wrap gap-10 mb-10">
+					<DonutCharts />
+				</div>
 
-      {/* Middle section: donut charts + scrollable table */}
-      <div className="flex-1 min-h-0 flex flex-col p-10">
-        {/* Donut Charts */}
-        <div className="flex flex-wrap gap-10 mb-10">
-          <DonutCharts />
-        </div>
+				<div className="flex-1 min-h-0 overflow-auto">
+					<TabularData />
+				</div>
+			</div>
 
-        {/* Scrollable Table */}
-        <div className="flex-1 min-h-0 overflow-auto">
-          <TabularData />
-        </div>
-      </div>
-
-      {/* Right-side panels */}
-      <div className="absolute right-0 top-6 translate-x-[100%]">
-        <FilterContainer />
-      </div>
-
-      <div className="absolute right-0 top-20 translate-x-[100%]">
-        <Methodology />
-      </div>
-    </div>
-  );
+			<div className="absolute right-0 top-10 translate-x-[100%]">
+				<Methodology />
+			</div>
+		</div>
+	);
 }
--- features/quick-view/components/stats/overall.tsx
+++ features/quick-view/components/stats/overall.tsx
@@ -1,212 +1,206 @@
-"use client";
+'use client';
 
-import { useState, useRef, useEffect } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { useRef, useEffect } from 'react';
 
-import { DonutChart } from "@/shared/ui/donut-chart";
+import { DonutChart } from '@/shared/ui/donut-chart';
+
+import { getTopNCasesByTotalCases } from '@/features/quick-view/lib/data-managers';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/shared/ui/table";
-import { Button } from "@/shared/ui/button";
-
-import { getTopNCasesByTotalCases } from "@/features/quick-view/lib/data-managers";
-import {
-  getTotalCases,
-  getTotalDeaths,
-  getTotalInjuries,
-  dataDistrictWiseInjuryDeath,
-} from "@/features/quick-view/lib/data-managers";
-import { useSelectedCasualtyStore } from "@/features/quick-view/store/selected-casualty-store";
+	getTotalCases,
+	getTotalDeaths,
+	getTotalInjuries,
+	dataDistrictWiseInjuryDeath,
+} from '@/features/quick-view/lib/data-managers';
+import { useSelectedCasualtyStore } from '@/features/quick-view/store/selected-casualty-store';
+import { Badge } from '@/shared/ui/badge';
 
 const total = getTotalCases();
 const deathCount = getTotalDeaths();
 const injuryCount = getTotalInjuries();
-const deathPercentage = ((deathCount / total) * 100).toFixed(1) + "%";
-const injuriesPercentage = ((injuryCount / total) * 100).toFixed(1) + "%";
 const topNCasesByTotalCases = getTopNCasesByTotalCases();
 
 const donutChartsConfig = () => {
-  return [
-    {
-      chart: {
-        data: [
-          { label: "Deaths", value: deathCount, color: "#9c0612" },
-          {
-            label: "Total Casualties",
-            value: total,
-            color: "#e2e0df",
-          },
-        ],
+	return [
+		{
+			chart: {
+				data: [
+					{ label: 'Deaths', value: deathCount, color: '#9c0612' },
+					{
+						label: 'Total Casualties',
+						value: total,
+						color: '#e2e0df',
+					},
+				],
 
-        size: 40,
-        thickness: 3,
-        innerText: "",
-      },
-      legend: {
-        label: "Deaths",
-        value: deathCount,
-      },
-    },
+				size: 40,
+				thickness: 3,
+				innerText: '',
+			},
+			legend: {
+				label: 'Deaths',
+				value: deathCount,
+			},
+		},
 
-    {
-      chart: {
-        data: [
-          { label: "Injuries", value: injuryCount, color: "#ee7f01" },
-          {
-            label: "Total Casualties",
-            value: total,
-            color: "#e2e0df",
-          },
-        ],
+		{
+			chart: {
+				data: [
+					{ label: 'Injuries', value: injuryCount, color: '#ee7f01' },
+					{
+						label: 'Total Casualties',
+						value: total,
+						color: '#e2e0df',
+					},
+				],
 
-        size: 40,
-        thickness: 3,
-        innerText: "",
-      },
-      legend: {
-        label: "Injuries",
-        value: injuryCount,
-      },
-    },
-  ];
+				size: 40,
+				thickness: 3,
+				innerText: '',
+			},
+			legend: {
+				label: 'Injuries',
+				value: injuryCount,
+			},
+		},
+	];
 };
 
 const TotalCasualties = () => {
-  return (
-    <div className="flex flex-col p-10">
-      <h5 className="text-xs w-max">Total Casualties</h5>
-      <p className="text-4xl font-semibold w-max">{total.toLocaleString()}</p>
-    </div>
-  );
+	return (
+		<div className="flex flex-col p-10">
+			<h5 className="text-xs w-max">Total Casualties</h5>
+			<p className="text-4xl font-semibold w-max">{total.toLocaleString()}</p>
+		</div>
+	);
 };
 
 const DonutCharts = () => {
-  return (
-    <>
-      {donutChartsConfig().map((donutChart) => (
-        <div
-          className="flex flex-wrap gap-2 items-center"
-          key={donutChart.legend.label}
-        >
-          <DonutChart
-            data={donutChart.chart.data}
-            size={donutChart.chart.size}
-            thickness={donutChart.chart.thickness}
-            innerText={donutChart.chart.innerText}
-          />
+	return (
+		<>
+			{donutChartsConfig().map((donutChart) => (
+				<div
+					className="flex flex-wrap gap-2 items-center"
+					key={donutChart.legend.label}
+				>
+					<DonutChart
+						data={donutChart.chart.data}
+						size={donutChart.chart.size}
+						thickness={donutChart.chart.thickness}
+						innerText={donutChart.chart.innerText}
+					/>
 
-          <div>
-            <h1 className="text-xs font-semibold">{donutChart.legend.value}</h1>
-            <p className="text-xs">{donutChart.legend.label}</p>
-          </div>
-        </div>
-      ))}
-    </>
-  );
+					<div>
+						<h1 className="text-xs font-semibold">{donutChart.legend.value}</h1>
+						<p className="text-xs">{donutChart.legend.label}</p>
+					</div>
+				</div>
+			))}
+		</>
+	);
 };
 
 const TabularData = () => {
-  const { selectedCasualty, toggleSelectedCasualty } =
-    useSelectedCasualtyStore();
-  const selectedRowRef = useRef<HTMLTableRowElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
+	const { selectedCasualty, toggleSelectedCasualty } =
+		useSelectedCasualtyStore();
+	const selectedRowRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (selectedRowRef.current && containerRef.current) {
-      const containerRect = containerRef.current.getBoundingClientRect();
-      const rowRect = selectedRowRef.current.getBoundingClientRect();
-      const scrollTop =
-        rowRect.top -
-        containerRect.top +
-        containerRef.current.scrollTop -
-        containerRect.height / 2;
-      containerRef.current.scrollTo({ top: scrollTop, behavior: "smooth" });
-    }
-  }, [selectedCasualty]);
+	useEffect(() => {
+		if (selectedRowRef.current && containerRef.current) {
+			const containerRect = containerRef.current.getBoundingClientRect();
+			const rowRect = selectedRowRef.current.getBoundingClientRect();
+			const scrollTop =
+				rowRect.top -
+				containerRect.top +
+				containerRef.current.scrollTop -
+				containerRect.height / 2;
+			containerRef.current.scrollTo({ top: scrollTop, behavior: 'smooth' });
+		}
+	}, [selectedCasualty]);
 
-  const displayData = dataDistrictWiseInjuryDeath;
+	const displayData = dataDistrictWiseInjuryDeath;
 
-  return (
-    <div ref={containerRef} className="overflow-auto h-full scrollbar-hide">
-      <Table className="min-w-full text-sm">
-        <TableHeader className="sticky top-0 bg-background z-10 text-xs">
-          <TableRow>
-            <TableHead>District</TableHead>
-            <TableHead>Total Cases</TableHead>
-            <TableHead>Deaths</TableHead>
-            <TableHead className="text-right">Injuries</TableHead>
-          </TableRow>
-        </TableHeader>
+	return (
+		<div ref={containerRef} className="overflow-auto h-full scrollbar-hide">
+			<div className="min-w-full text-sm">
+				<div>
+					{displayData
+						.sort((a, b) => (b.total_cases || 0) - (a.total_cases || 0))
+						.map((item, index, arr) => {
+							const selected = selectedCasualty?.district === item.district;
+							const nextSelected =
+								index + 1 < arr.length &&
+								selectedCasualty?.district === arr[index + 1].district;
 
-        <TableBody>
-          {displayData
-            .sort((a, b) => (b.total_cases || 0) - (a.total_cases || 0))
-            .map((item) => {
-              const selected = selectedCasualty?.district === item.district;
-              return (
-                <TableRow
-                  key={item.district}
-                  ref={selected ? selectedRowRef : null}
-                  className={`cursor-pointer ${selected ? "bg-accent" : ""}`}
-                  onClick={() => toggleSelectedCasualty(item)}
-                >
-                  <TableCell>{item.district}</TableCell>
-                  <TableCell>{item.total_cases}</TableCell>
-                  <TableCell>{item.verified_deaths}</TableCell>
-                  <TableCell className="text-right">
-                    {item.verified_injuries}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-        </TableBody>
-      </Table>
-    </div>
-  );
+							const shouldHaveBorder = !selected && !nextSelected;
+
+							return (
+								<div
+									key={item.district}
+									ref={selected ? selectedRowRef : null}
+									className={`cursor-pointer w-full hover:bg-slate-200 flex justify-between items-center px-4 py-4 ${
+										selected
+											? 'bg-foreground font-semibold px-4 shadow-md rounded-3xl border border-slate-100'
+											: ''
+									} ${shouldHaveBorder ? 'border-b' : ''}`}
+									onClick={() => toggleSelectedCasualty(item)}
+								>
+									<div>{item.district}</div>
+
+									<div className="flex flex-wrap gap-2">
+										<Badge className="bg-[#970811] text-white">
+											{item.verified_deaths}
+										</Badge>
+										<Badge className="bg-[#ef7b0a] text-white">
+											{item.verified_injuries}
+										</Badge>
+									</div>
+								</div>
+							);
+						})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const TopNCasesByTotalCases = () => {
-  const maxCases = Math.max(
-    ...topNCasesByTotalCases.map((item) => item.total_cases || 0)
-  );
+	const maxCases = Math.max(
+		...topNCasesByTotalCases.map((item) => item.total_cases || 0),
+	);
 
-  return (
-    <div className="flex flex-col gap-2">
-      <span className="font-bold uppercase text-xs font-mono">
-        Most Effected
-      </span>
-      {topNCasesByTotalCases.map((item, index) => {
-        const widthPercentage = Math.max(
-          10,
-          Math.min(100, ((item.total_cases || 0) / maxCases) * 100)
-        );
+	return (
+		<div className="flex flex-col gap-2">
+			<span className="font-bold uppercase text-xs font-mono">
+				Most Effected
+			</span>
+			{topNCasesByTotalCases.map((item, index) => {
+				const widthPercentage = Math.max(
+					10,
+					Math.min(100, ((item.total_cases || 0) / maxCases) * 100),
+				);
 
-        const colors = ["bg-indigo-600", "bg-indigo-500", "bg-indigo-400"];
+				const colors = ['bg-indigo-600', 'bg-indigo-500', 'bg-indigo-400'];
 
-        const bgColor = colors[index % colors.length];
+				const bgColor = colors[index % colors.length];
 
-        return (
-          <div key={item.district} className="flex gap-2 items-center">
-            <div
-              key={item.district}
-              className={`flex rounded-3xl items-center justify-between px-4 py-2 ${bgColor}`}
-              style={{ width: `${widthPercentage}%` }}
-            >
-              <span className="font-medium text-white">{item.district}</span>
-            </div>
-            <span className="font-normal whitespace-nowrap">
-              {item.total_cases?.toLocaleString()} Cases
-            </span>
-          </div>
-        );
-      })}
-    </div>
-  );
+				return (
+					<div key={item.district} className="flex gap-2 items-center">
+						<div
+							key={item.district}
+							className={`flex rounded-3xl items-center justify-between px-4 py-2 ${bgColor}`}
+							style={{ width: `${widthPercentage}%` }}
+						>
+							<span className="font-medium text-white">{item.district}</span>
+						</div>
+						<span className="font-normal whitespace-nowrap">
+							{item.total_cases?.toLocaleString()} Cases
+						</span>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export { TotalCasualties, DonutCharts, TabularData, TopNCasesByTotalCases };
