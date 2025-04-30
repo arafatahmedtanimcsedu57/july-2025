941dbb4138d96efd2046c12ef0a27d48886f880e
Arafat <arafatahmedtanimcsedu57@gmail.com>
2025-04-30 07:53:53 +0600

    update

--- features/team/data.ts
+++ features/team/data.ts
@@ -1,10 +1,14 @@
 export const team = [
-	{ name: 'Sabhanaz Rashid Diya', des: 'Supervisor', bio: '' },
+	{
+		name: 'Sabhanaz Rashid Diya',
+		des: 'Supervisor',
+		bio: 'Sabhanaz Rashid Diya is the executive director at Tech Global Institute, and a computational social scientist specializing in technology governance and human rights in the Global Majority. With over 18 years of experience, her work focuses on the intersection of algorithmic accountability, data governance, and the sociopolitical and human rights impacts of emerging technologies. Diya started her career as a journalist in Dhaka, and has led multiple efforts on democracy, digital rights and social justice in Bangladesh and worldwide. She holds a Master’s in Public Policy and Data Science from UC Berkeley, and is a visiting policy fellow at the University of Oxford. Diya’s work has been published and quoted by The Financial Times, Wired, BBC, Al Jazeera, Reuters, Rest of World and several international and regional media. ',
+	},
 
 	{
 		name: 'Sams Wahid Shahat',
 		des: 'Researcher and Project Coordinator',
-		bio: '',
+		bio: 'Sams Wahid Shahat is a researcher at Tech Global Institute specializing in information integrity and digital forensics. He serves as the project coordinator of the Monsoon Revolution Archive, where he documents and verifies evidence of human rights violations during Bangladesh’s 2024 July Uprising. Sams began his career at FactWatch—Bangladesh’s first verified signatory of the International Fact-Checking Network (IFCN). At Tech Global Institute, he leads on archival planning and partnerships, and contributes to media analysis, internet research, and open-source investigations to strengthen efforts around truth, justice, and accountability in the Global Majority.',
 	},
 	{
 		name: 'Apon Das',
--- features/team/team-container.tsx
+++ features/team/team-container.tsx
@@ -4,25 +4,81 @@ const TeamContainer = () => {
 	return (
 		<section className="px-10 py-16 bg-foreground  text-slate-700">
 			<div className="container mx-auto">
-				<div className="flex justify-between items-center mb-6">
-					<h2 className="text-2xl font-bold text-slate-600">About</h2>
+				<div className="flex flex-col justify-between mb-16">
+					<h2 className="text-2xl font-bold text-slate-600">About Us</h2>
+					<p>
+						Our team consists of experienced researchers and investigators with
+						background in international human rights law, open-source
+						investigations, journalism, and media research. In addition to team
+						members listed below, we work with a wide network of specialists and
+						experts in criminal law, archiving, forensics, UN procedures, and
+						other related fields.
+					</p>
 				</div>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-					{' '}
-					{team.map((member) => {
-						return (
-							<div className="p-4 mb-16">
-								<h3 className="text-2xl font-semibold">{member.name}</h3>
-								<br />
-								<h4 className="text-xl font-medium">{member.des}</h4>
-								<br />
-								<br />
-								<br />
-								<p>{member.bio}</p>
-							</div>
-						);
-					})}
+				<div className="flex flex-col justify-between  mb-16">
+					<h2 className="text-2xl font-bold text-slate-600">Our Goals</h2>
+					<ul>
+						<li>
+							<strong>Document and Verify Abuses:</strong> Systematically
+							collect and authenticate open-source and eyewitness evidence of
+							human rights violations by state-sanctioned actors, including law
+							enforcement.
+						</li>
+						<li>
+							<strong>Ensure Secure Preservation:</strong> Create a protected,
+							accessible digital archive to store verified evidence,
+							safeguarding it from loss, tampering, or erasure.
+						</li>
+						<li>
+							<strong>Advance Justice and Accountability:</strong> Share
+							credible findings with independent judicial bodies to support
+							legal action and redress for victims in accordance with
+							international human rights and criminal law, and safeguards
+							outlined under the{' '}
+							<a href="https://www.ohchr.org/en/publications/policy-and-methodological-publications/berkeley-protocol-digital-open-source">
+								Berkeley Protocol
+							</a>
+							.
+						</li>
+						<li>
+							<strong>Support Research and Advocacy:</strong> Provide vetted
+							materials to independent researchers, journalists, and human
+							rights advocates to inform public discourse and drive policy
+							change.
+						</li>
+						<li>
+							<strong>Strengthen Documentation Capacity:</strong> Train human
+							rights defenders in open-source investigation and digital
+							archiving to expand accountability efforts.
+						</li>
+						<li>
+							<strong>Preserve Public Memory:</strong> Maintain an accurate
+							record of the July Uprising in Bangladesh to honor victims and
+							resist historical revisionism.
+						</li>
+					</ul>
+				</div>
+
+				<div className="flex flex-col justify-between mb-16">
+					<h2 className="text-2xl font-bold text-slate-600">Project Team</h2>
+
+					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+						{' '}
+						{team.map((member) => {
+							return (
+								<div className="py-6 mb-16">
+									<h3 className="text-2xl font-semibold">{member.name}</h3>
+									<br />
+									<h4 className="text-xl font-medium">{member.des}</h4>
+									<br />
+									<br />
+									<br />
+									<p>{member.bio}</p>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 		</section>
