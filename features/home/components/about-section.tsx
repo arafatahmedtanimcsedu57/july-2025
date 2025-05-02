a1f77cc576bc89dc8aa90ba60285db0cc009b694
Arafat <arafatahmedtanimcsedu57@gmail.com>
2025-05-02 21:46:44 +0600

    update

--- features/home/components/about-section.tsx
+++ features/home/components/about-section.tsx
@@ -3,40 +3,24 @@ import { aboutData } from '../data';
 function AboutSection() {
 	return (
 		<section id="about" className="py-16 text-primary">
-			<div className="container mx-auto">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-16 px-6">
+			<div className="container mx-auto px-10">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-16">
 					{aboutData.map((section, idx) => (
-						<div
-							key={idx}
-							className={`lg:grid lg:grid-cols-3 ${
-								idx / 2 !== 0 ? 'text-accent' : ''
-							}`}
-						>
+						<div key={idx} className={`${idx / 2 !== 0 ? 'text-accent' : ''}`}>
 							<h2 className="text-2xl leading-8 font-bold mb-8 text-muted-foreground">
 								{section.heading}
 							</h2>
-							{idx === 0
-								? // Use blockquote for the first section
+							{idx === 0 ? (
+								// Use blockquote for the first section
 
-								  section.paragraphs.map((para, pIdx) => (
-										<>
-											{' '}
-											<p
-												key={pIdx}
-												className="col-span-2 mb-4 text-secondary-foreground leading-8" // Added underline style
-											>
-												{para}
-											</p>
-											<br />
-											<br />
-											<div className="rounded-full h-[2px] w-full animate-glow bg-[linear-gradient(90deg,#666666,#666666,#f0f0f0,#666666)] bg-[length:200%_100%]" />
-										</>
-								  ))
-								: section.paragraphs.map((para, pIdx) => (
+								<div className="relative z-10">
+									{' '}
+									{/* Ensure content is above quote mark */}
+									{section.paragraphs.map((para, pIdx) => (
 										<>
 											<p
 												key={pIdx}
-												className="col-span-2 mb-4 text-secondary-foreground leading-8"
+												className="mb-4 text-secondary-foreground leading-8" // Added underline style
 											>
 												{para}
 											</p>
@@ -45,7 +29,24 @@ function AboutSection() {
 											<br />
 											<div className="rounded-full h-[2px] w-full animate-glow bg-[linear-gradient(90deg,#666666,#666666,#f0f0f0,#666666)] bg-[length:200%_100%]" />
 										</>
-								  ))}
+									))}
+								</div>
+							) : (
+								section.paragraphs.map((para, pIdx) => (
+									<>
+										<p
+											key={pIdx}
+											className="mb-4 text-secondary-foreground leading-8"
+										>
+											{para}
+										</p>
+
+										<br />
+										<br />
+										<div className="rounded-full h-[2px] w-full animate-glow bg-[linear-gradient(90deg,#666666,#666666,#f0f0f0,#666666)] bg-[length:200%_100%]" />
+									</>
+								))
+							)}
 						</div>
 					))}
 				</div>
--- features/home/components/important-section.tsx
+++ features/home/components/important-section.tsx
@@ -2,36 +2,42 @@ import { importantData, archiveData } from '../data';
 
 function ImportantSection() {
 	return (
-		<section className="text-primary">
-			<div className="container mx-auto">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-16 px-6">
+		<section className=" mb-16 dark:text-white text-primary">
+			<div className="container mx-auto px-10">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-16">
 					{importantData.map((section, idx) => (
-						<div
-							key={idx}
-							className={`lg:grid lg:grid-cols-3 ${
-								idx % 2 === 0 ? 'text-accent' : ''
-							}`}
-						>
-							<h2
-								className={`text-2xl leading-8 font-bold mb-8 text-destructive`}
-							>
-								{section.heading}
-							</h2>
-							{section.type === 'paragraphs' &&
-								section.content.map((para, pIdx) => (
-									<>
-										{' '}
+						<div key={idx} className={`${idx % 2 === 0 ? 'text-accent' : ''}`}>
+							<div>
+								<h2
+									className={`text-2xl leading-8 font-bold mb-8  ${
+										idx === importantData.length - 1
+											? 'text-destructive'
+											: 'text-muted-foreground'
+									}`}
+								>
+									{section.heading}
+								</h2>
+								{section.type === 'paragraphs' &&
+									section.content.map((para, pIdx) => (
 										<p
 											key={pIdx}
-											className="col-span-2 mb-4 text-secondary-foreground leading-8"
+											className="mb-4 text-secondary-foreground leading-8"
 										>
 											{para}
 										</p>
-										<br />
-										<br />
-										<div className="rounded-full h-[2px] w-full animate-glow bg-[linear-gradient(90deg,#666666,#666666,#f0f0f0,#666666)] bg-[length:200%_100%]"></div>
-									</>
-								))}
+									))}
+							</div>
+
+							{idx % 2 === 0 ? (
+								<>
+									{' '}
+									<br />
+									<br />
+									<div className="rounded-full h-[2px] w-full animate-glow bg-[linear-gradient(90deg,#666666,#666666,#f0f0f0,#666666)] bg-[length:200%_100%]"></div>
+								</>
+							) : (
+								<></>
+							)}
 						</div>
 					))}
 				</div>
